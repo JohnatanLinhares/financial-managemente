95cc22788646ad973ebdc6b8f83964cc289d3881
johnatan Linhares <johnatan.s.linhares@gmail.com>
2023-06-07 20:24:56 -0300

    gestao-#002

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import  './App.css';
+
 
 function App() {
+  const [saldo, setSaldo] = useState(0);
+  const [valorEntrada, setValorEntrada] = useState('');
+  const [valorSaida, setValorSaida] = useState('');
+
+  const handleValorEntradaChange = (event) => {
+    setValorEntrada(event.target.value);
+  };
+
+  const handleValorSaidaChange = (event) => {
+    setValorSaida(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const novoSaldo = saldo + (valorEntrada - valorSaida);
+    setSaldo(novoSaldo);
+    setValorEntrada('');
+    setValorSaida('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sistema de Gestão Financeira</h1>
+      <p>Saldo: R${saldo.toFixed(2)}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Valor de Entrada:
+          <input
+            type="number"
+            value={valorEntrada}
+            onChange={handleValorEntradaChange}
+          />
+        </label>
+        <br />
+        <label>
+          Valor de Saída:
+          <input
+            type="number"
+            value={valorSaida}
+            onChange={handleValorSaidaChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Adicionar</button>
+      </form>
     </div>
   );
 }
